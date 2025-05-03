7c47648dac14f23492d55e184d9e05457ef336cb
sjpark0 <sjpark0@gmail.com>
2025-05-03 14:03:14 +0900

    scview.sh추가

--- scview.sh
+++ scview.sh
@@ -1,4 +1,5 @@
 workdir=$1
-workdir2=$2
-
-SET=$(seq 0 300)
+colmap feature_extractor --database_path $workdir/database.db --image_path $workdir/images  --SiftExtraction.max_image_size 4096 --SiftExtraction.max_num_features 16384 --SiftExtraction.estimate_affine_shape 1 --SiftExtraction.domain_size_pooling 1
+colmap exhaustive_matcher --database_path $workdir/database.db
+mkdir -p $workdir/sparse/0
+colmap mapper --database_path $workdir/database.db --image_path $workdir/images --output_path $workdir/sparse
