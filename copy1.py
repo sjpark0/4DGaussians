d38fc180382eb8dac8a758e5ba46d5185cd03806
sjpark0 <sjpark0@gmail.com>
2025-05-19 11:12:59 +0900

    modify

--- copy1.py
+++ copy1.py
@@ -10,9 +10,8 @@ folder_list = os.listdir(workdir)
 #folder_list = sorted(folder_list)
 folder_list = [int(folder) for folder in folder_list if not folder.startswith("Param")]
 folder_list.sort()
-i = 503
-for folder in folder_list:
-    path = os.path.join(workdir, str(folder), "images")
+for i in range(502, 1571, 1):
+    path = os.path.join(workdir, str(folder_list[i]), "images")
     file_list = os.listdir(path)
     file_list.sort()
     #file_list = sorted(file_list)
@@ -20,15 +19,11 @@ for folder in folder_list:
         if file_list[m].endswith(".png"):
             #os.makedirs(os.path.join(workdir1, "cam{0:02d}".format(m+1)), exist_ok=True)
             filename1 = os.path.join(path, file_list[m])
-            filename2 = os.path.join(workdir1, "cam{0:02d}".format(m+1), "frame_{0:05d}.jpg".format(i - 503 + 1))
+            filename2 = os.path.join(workdir1, "cam{0:02d}".format(m+1), "frame_{0:05d}.jpg".format(i - 502 + 1))
             img = cv2.imread(filename1)
             img1 = cv2.resize(img, dsize=(960, 540))
             cv2.imwrite(filename2, img1)
 
             #print("cp " + os.path.join(path, file_list[m]) + " " + os.path.join(workdir1, "cam{0:02d}".format(m), "images", "{0:04d}.png".format(i)))
             #os.system("cp " + os.path.join(path, file_list[m]) + " " + os.path.join(workdir1, "cam{0:02d}".format(m+1), "frame_{0:04d}.png".format(i - 503 + 1)))
-    i += 1
-    if i == 1571:
-        break
-        
-        
+    
